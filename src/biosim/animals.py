669c4c8adf09b80d2290110d95d467a53e2124cf
Helene Olsen <helene.drennan.olsen@nmbu.no>
2022-01-04 23:11:36 +0100

    Making alternative fitness function that doesn't work,  changing update_fodder in the landscape and adding an eating_fodder function. Also adding some notes on what to do further in landscape.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -6,18 +6,15 @@ import math
 from general import animal_const, getnestdict
 
 
-
-
-
 class herbivore:
 
-    def __init__(self, age = None, weight = None, ):
+    def __init__(self, age=None, weight=None):
         self.classname = self.__class__.__name__
 
         self.age = age if age is not None else 0
         random_weight = random.gauss(getnestdict(animal_const, self.classname, 'w_birth'), getnestdict(animal_const, self.classname, 'sigma_birth'))
         self.weight = weight if weight is not None else random_weight
-        self.fitness = update_fitness(self)
+        self.fitness = None
 
     def update_age(self, years=None):
         self.age += years if years is not None else 1
@@ -26,6 +23,25 @@ class herbivore:
         beta = getnestdict(animal_const, self.classname, 'beta')
         eta = getnestdict(animal_const, self.classname, 'eta')
         self.weight = (beta*fooder) - (eta*self.weight)
+
+#    def update_fitness(self):
+#        a_half = getnestdict(animal_const, self.classname, 'a_half')
+#        phi_age = getnestdict(animal_const, self.classname, 'phi_age')
+#        w_half = getnestdict(animal_const, self.classname, 'w_half')
+#        phi_weight = getnestdict(animal_const, self.classname, 'phi_weight')
+#
+#        q_pos = (1/(1 + math.exp(phi_age*(self.age - a_half))))
+#        q_neg = (1/(1 + math.exp(-phi_weight*(self.weight - w_half))))
+#
+#        if self.weight <= 0:
+#            self.fitness = 0
+#
+#        else:
+#            self.fitness = q_pos * q_neg
+#
+#        if self.fitness > 1:
+#            self.fitness = 1
+
 '''
     def update_fitness(self):
         def q(x, x_half, phi, sign):
@@ -50,5 +66,4 @@ for year in range(10):
     print(f'Year: {year}, age is {h1.age} and weight is {h1.weight}')
     h1.update_age()
     h1.update_weight(fooder=13)
-
-
+    h1.update_fitness()
--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -3,7 +3,6 @@ Template for Landscape class.
 """
 
 from .general import animal_const, landscape_const, getnestdict
-from .animal import
 
 class Lowland:
     """
@@ -13,9 +12,26 @@ class Lowland:
     def __init__(self, fodder=None, pop_herb=None):
         self.classname = self.__class__.__name__
         self.fodder = fodder if fodder is not None else getnestdict(landscape_const, self.classname, 'f_max')
+        # Needs revising ^
+        self.herbivore_population = #Input herbivore population in a tile?
 
     def update_fodder(self):
         herbivore_portion = getnestdict(animal_const, 'herbivore', 'F')
-        self.fodder = self.fodder - herbivore_portion
+
+        if self.fodder >= herbivore_portion:
+            self.fodder -= herbivore_portion
+
+        elif 0 < self.fodder < herbivore_portion:
+            self.fodder = 0
+            rest_fodder = self.fodder #? Restene av maten om det er under 10
+
+        else:
+            pass #?
+
+
+    def eat_fodder(self):
+        #Each herbuvore in sorted fitness list eats off available fodder and adds weight
+
+
 
 # def regrow_fodder(self, self.year): Til senere under generell Landscape

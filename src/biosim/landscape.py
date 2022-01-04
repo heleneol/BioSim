b6ead37261ad06d9c3758f8bff31c0ce90dd1e42
Helene Olsen <helene.drennan.olsen@nmbu.no>
2022-01-04 15:01:22 +0100

    Changing population to herbivore population as we'll have to differentiate between species population later.

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -7,7 +7,7 @@ from .general import landscape_const, getnestdict
 
 class Lowland:
 
-    def __init__(self, fodder=None, pop=None):
+    def __init__(self, fodder=None, pop_herb=None):
         self.classname = self.__class__.__name__
         self.fodder = fodder if fodder is not None else getnestdict(landscape_const, self.classname, 'f_max')
-        self.pop = pop if pop is not None else getnestdict(placement, len('pop'))
+        self.pop_herb = pop_herb if pop_herb is not None else getnestdict(Xsomelist, len(pop))

6a1a3c1af34866169af958d34e259d0a6621d2d2
Johannes Fjeldså <johannes.larsen.fjeldsa@nmbu.no>
2022-01-04 16:48:33 +0100

    problems concerning making a common methode for updating fitness in year = 0 and per year.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -6,16 +6,18 @@ import math
 from general import animal_const, getnestdict
 
 
+
+
+
 class herbivore:
 
-    def __init__(self, age = None, weight = None):
+    def __init__(self, age = None, weight = None, ):
         self.classname = self.__class__.__name__
 
         self.age = age if age is not None else 0
         random_weight = random.gauss(getnestdict(animal_const, self.classname, 'w_birth'), getnestdict(animal_const, self.classname, 'sigma_birth'))
         self.weight = weight if weight is not None else random_weight
-
-        self.fitness = 1 # dette er random
+        self.fitness = update_fitness(self)
 
     def update_age(self, years=None):
         self.age += years if years is not None else 1
@@ -24,14 +26,13 @@ class herbivore:
         beta = getnestdict(animal_const, self.classname, 'beta')
         eta = getnestdict(animal_const, self.classname, 'eta')
         self.weight = (beta*fooder) - (eta*self.weight)
-
+'''
     def update_fitness(self):
-
         def q(x, x_half, phi, sign):
             if sign == 'pos':
                 return (1 / (1 + (math.exp(phi * (x - x_half)))))
             elif sign == 'neg':
-                return (1 / (1 + (math.exp(-phi * (x - x_half)))))
+                return (1 / (1 + (math.exp((-1) * phi * (x - x_half)))))
 
         if self.weight <= 0:
             self.fitness = 0
@@ -42,12 +43,12 @@ class herbivore:
             phi_weight = getnestdict(animal_const, self.classname, 'phi_weight')
             self.fitness = (q(self.age, a_half, phi_age, 'pos') * q(self.weight, w_half, phi_weight, 'neg'))
             if self.fitness > 1:
-                self.fitness = 1
+                self.fitness = 1'''
 
 h1 = herbivore()
 for year in range(10):
     print(f'Year: {year}, age is {h1.age} and weight is {h1.weight}')
     h1.update_age()
-    h1.update_weight(fooder=10)
-    h1.update_fitness()
+    h1.update_weight(fooder=13)
+
 

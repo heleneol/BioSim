df1df5421fc14cd8e1b904c3b4dc7e4344b8ac38
Helene Olsen <helene.drennan.olsen@nmbu.no>
2022-01-04 13:59:52 +0100

    Introducing Landscape class, and importing constants&functions from general file

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -2,3 +2,12 @@
 Template for Landscape class.
 """
 
+from general import landscape_const
+from general import getnestdict
+
+
+class Lowland:
+
+    def __init__(self, fodder=None):
+        self.classname = self.__class__.__name__
+        self.fodder = fodder if fodder is not None else getnestdict(landscape_const, self.classname, 'f_max')

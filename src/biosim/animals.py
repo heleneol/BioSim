9cb03f0b5ea15e23d512138e502e7dde947b3a57
Johannes Fjeldså <johannes.larsen.fjeldsa@nmbu.no>
2022-01-04 13:47:15 +0100

    made dict with animal constants with function for getting values from nested dictionaries. Made parameteres self.age using 0 as default and self.weight using random number in gausian distrubution as default

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -38,17 +38,34 @@ def getnestdict(dict, key1, key2):
     try:
         return dict.get(key1, {}).get(key2)
     except KeyError:
+        print('Keyvalue does not exist')
         return None
 
 
 
 class herbivore:
-    from random import random.gauss
+
 
     def __init__(self, age = None, weight = None):
         self.classname = self.__class__.__name__
         self.age = age if age is not None else 0
         random_weight = random.gauss(getnestdict(animal_const, self.classname, 'w_birth'), getnestdict(animal_const, self.classname, 'sigma_birth'))
         self.weight = weight if weight is not None else random_weight
-        self.fitness =
+
+    def update_age(self):
+        self.age += 1
+
+    def update_weight(self):
+        a = 2
+
+
+
+    def update_fitness(self):
+        a = 2
+
+
+h1 = herbivore(age= 2, weight= 4)
+print(f'age = {h1.age} and weight = {h1.weight}')
+
+
 

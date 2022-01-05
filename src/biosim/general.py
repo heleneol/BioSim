35b5a3536611724d5746926c7dd562a9bee3d81b
Johannes Fjeldså <johannes.larsen.fjeldsa@nmbu.no>
2022-01-05 15:05:56 +0100

    deleted general.py

--- src/biosim/general.py
+++
@@ -1,9 +0,0 @@
-
-
-landscape_const = {'lowland': {'f_max': 800},
-                   'highland': {'f_max': 300},
-                   'water': {'f_max': 0},
-                   'desert': {'f_max': 0}}
-
-
-

51cba396f191b1093ce4f1f3d3fa92ce97ed9b9d
Helene Olsen <helene.drennan.olsen@nmbu.no>
2022-01-08 18:57:37 +0100

    Setting up a basic setup py-file.

---
+++ setup.py
@@ -0,0 +1,6 @@
+# All necessary information is provided in setup.cfg.
+# Having this file allows editable installs, see
+# https://setuptools.readthedocs.io/en/latest/userguide/quickstart.html#id10
+
+import setuptools
+setuptools.setup()
